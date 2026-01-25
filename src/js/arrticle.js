4202074ead8237558bd837666809cf45c559521a
Ng ShengKee <ngshengkee@gmail.com>
2026-01-25 15:15:30 +0800

    feat: add carousel

--- src/js/arrticle.js
+++ src/js/arrticle.js
@@ -1,1 +1,13 @@
 import '../css/arrticle.css';
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
