add8844b2663f78c5d70fbed298c0dd60683ba4a
Ng ShengKee <ngshengkee@gmail.com>
2026-01-25 21:43:41 +0800

    feat

--- src/js/arrticle.js
+++ src/js/arrticle.js
@@ -8,12 +8,19 @@ const Carousel = {
 
         $elements.each((_, element) => {
             let config = $(element).data('config') || {};
+            let $childs = $(element).find('.owl-item');
+            
+            if ($childs.length <= 1) {
+                $(element).addClass('owl-loaded').addClass('is-alone');
+                return;
+            };
     
             $(element).owlCarousel({
                 loop: true,
                 margin: 0,
                 nav: false,
                 dots: true,
+                autoHeight:true,
                 navText: [`
                     <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 2L2 10L10 18" stroke="#005EAA" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
