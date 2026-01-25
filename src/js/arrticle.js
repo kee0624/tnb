ee0a6ebd8b47e780e9829ddaaf6e807ace937496
Ng ShengKee <ngshengkee@gmail.com>
2026-01-25 18:15:25 +0800

    feat: clean up

--- src/js/arrticle.js
+++ src/js/arrticle.js
@@ -10,10 +10,10 @@ const Carousel = {
             let config = $(element).data('config') || {};
     
             $(element).owlCarousel({
-                loop: false,
+                loop: true,
                 margin: 0,
-                nav: true,
-                dots: false,
+                nav: false,
+                dots: true,
                 navText: [`
                     <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 2L2 10L10 18" stroke="#005EAA" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
