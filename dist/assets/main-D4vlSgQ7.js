86e57307d4d50e3d3fba31959f1b1190df6de58c
Ng ShengKee <ngshengkee@gmail.com>
2026-01-26 22:00:42 +0800

    feat: update

---
+++ dist/assets/main-D4vlSgQ7.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";const o={init(){$("#videoModal").on("hidden.bs.modal",function(){const i=$(this).find("iframe"),t=i.attr("src");i.attr("src",t)})}};o.init();
