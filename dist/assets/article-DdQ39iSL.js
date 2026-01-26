fedd8d055c6e10a8cd75c81b59b79e56ca771d47
Ng ShengKee <ngshengkee@gmail.com>
2026-01-26 20:48:22 +0800

    feat: update

--- dist/assets/article-DdQ39iSL.js
+++
@@ -1,7 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const s={init(){const o=$(".owl-carousel");o.length!==0&&o.each((n,t)=>{let e=$(t).data("config")||{};if($(t).find(".item").length<=1){$(t).addClass("owl-loaded").addClass("is-alone");return}$(t).owlCarousel({loop:!0,margin:0,nav:!1,dots:!0,autoHeight:!0,navText:[`
-                    <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 2L2 10L10 18" stroke="#005EAA" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    `,`<svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 2L2 10L10 18" stroke="#005EAA" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>`],responsive:{0:{items:1}},...e})})}},i={init(){$("body").scrollspy({target:".fuse-article-aside .navbar"})}};s.init();i.init();
